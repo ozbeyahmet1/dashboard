a051b01c15515b02fd8ab9d2d2c6fc46262b218c
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-27 13:43:38 +0300

    feat: creates head component

---
+++ src/components/head.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+
+export const HeadComponent = () => {
+  return (
+    <Head>
+      {/* Meta tags */}
+      <title>Innoloft | B2B platform solution</title>
+      <meta name="description" content="Innoloft | B2B platform solution" />
+      <meta
+        name="keywords"
+        content="platform building, PaaS, LoftOS, network, marketplace, development tools, cloud-based, creation, online, management"
+      />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" href="/metalogo.jpg" />
+
+      {/* Open Graph tags */}
+      {/* Open Graph is a technology that allows web pages to become more rich and engaging when shared on social media platforms such as Facebook, Twitter, and LinkedIn. By adding Open Graph meta tags to your web page, you can control how your page appears when it's shared on social media, including the title, description, and image that are displayed. */}
+      <meta property="og:title" content="Innoloft | B2B platform solution" />
+      <meta property="og:description" content=" B2B platform solution" />
+      <meta property="og:image" content="" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter tags */}
+      {/* Twitter meta tags are similar to Open Graph tags, but are used specifically for Twitter. They allow you to control how your web page appears when it's shared on Twitter, including the title, description, and image that are displayed. */}
+      <meta name="twitter:title" content="Innoloft | B2B platform solution" />
+      <meta name="twitter:description" content=" B2B platform solution" />
+    </Head>
+  );
+};
