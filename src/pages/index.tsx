0975ed44d4f893241ff4b44cda19af78cf91ae26
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-27 14:19:56 +0300

    feat: fetchs data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div className="text-gray">asdsaasdasda</div>;
+import { Product } from "@/interface/product";
+import { getSingleProductData } from "@/utils";
+import { GetStaticProps } from "next";
+
+type Props = {
+  readonly product: Product;
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const endpoint = params?.id ? params.id.toString() : "6781";
+  const product: Product = await getSingleProductData(endpoint);
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+export default function Home({ product: { name } }: Props) {
+  return (
+    <div>
+      <div className="flex w-full">
+        <div>{name}</div>
+      </div>
+    </div>
+  );
 }
---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { Product } from "./interface/product";
+
+export const INNOLOFT_API_ENDPOINT = "https://api-test.innoloft.com/";
+
+export async function getSingleData<T>(endpoint: string): Promise<T> {
+  try {
+    const res: AxiosResponse<T> = await axios.get(endpoint);
+    return res.data;
+  } catch (error: AxiosError | unknown) {
+    console.log((error as AxiosError).message);
+    return null as unknown as T;
+  }
+}
+
+export const getSingleProductData = (endpoint: string) =>
+  getSingleData<Product>(INNOLOFT_API_ENDPOINT + "product/" + endpoint);
