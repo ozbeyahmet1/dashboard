e5000ababfe99340ff1f6262b9a33bbc067fd682
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2023-04-27 14:00:54 +0300

    feat: changes font importing location

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,18 @@
+import { Lexend } from "next/font/google";
 import { PropsWithChildren } from "react";
 import { HeadComponent as Head } from "./head";
 import { Header } from "./header";
 import Sidebar from "./sidebar";
-
+const lexend = Lexend({ subsets: ["latin"] });
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="flex h-full min-h-screen w-full items-start">
+    <div className={`flex h-full min-h-screen w-full items-start ${lexend.className}`}>
       <Head />
       <Header />
       <div className="hidden lg:block">
         <Sidebar />
       </div>
-      <main className="w-full pt-16 lg:ml-[8.5%] lg:w-11/12 xl:ml-[20%] xl:w-4/5">{children}</main>
+      <main className="mt-20 w-full p-8 lg:ml-[8.5%] lg:w-11/12 xl:ml-[20%] xl:w-4/5">{children}</main>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-import { Lexend } from "next/font/google";
 
-const lexend = Lexend({ subsets: ["latin"] });
+
+
 
 export default function Home() {
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${lexend.className} `}></main>;
+  return (
+    <div className="text-gray">
+      asdsaasdasda
+    </div>);
 }
